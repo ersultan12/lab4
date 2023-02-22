739321a2979c3f48da46626bb0a2d532c9149f9f
ersultan12 <50986852+ersultan12@users.noreply.github.com>
2023-02-22 23:54:33 +0600

    3rd commit

--- src/app/products.ts
+++ src/app/products.ts
@@ -10,28 +10,84 @@ export interface Product {
 export const products = [
   {
     id: 1,
-    name: 'Phone XL',
-    price: 799,
+    name: 'Iphone 13 pro MAX 256gb zolotoy',
+    price: 676000,
     description: 'технология NFC: Дацвет: золотистыйтип экрана: OLED, Super Retina XDR диагональ: 6.1 дюйм размер оперативной памяти: 6 ГБ процессор: 6-ядерный Apple A15 Bionic объем встроенной памяти: 256 ГБ емкость аккумулятора: 3095 мАч',
     url: 'https://kaspi.kz/shop/p/apple-iphone-13-pro-256gb-zolotistyi-102298726/?c=750000000#!/item',
     img_url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h14/h34/46393533235230/apple-iphone-13-pro-256gb-zolotistyj-102298726-1-Container.jpg'
   },
   {
     id: 2,
-    name: 'Phone Mini',
-    price: 699,
-    description: 'A great phone with one of the best cameras',
-    url: '',
-    img_url: ''
+    name: 'Ноутбук ASUS TUF F15 FX506HM-HN016 90NR0754-M003E0 черный',
+    price: 639990, 
+    description: 'диагональ экрана: 15.6 дюйм процессор: Intel Core i5-11400H видеокарта: NVIDIA GeForce RTX 3060 размер оперативной памяти: 16 ГБ тип жесткого диска: SSD общий объем накопителей: 512 ГБ',
+    url: 'https://kaspi.kz/shop/p/asus-tuf-f15-fx506hm-hn016-90nr0754-m003e0-chernyi-103394916/?c=750000000#!/item',
+    img_url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hfd/h75/49038469234718/asus-tuf-f15-fx506hm-hn016-90nr0754-m003e0-cernyj-103394916-1.jpg'
   },
   {
     id: 3,
-    name: 'Phone Standard',
-    price: 299,
-    description: '',
-    url: '',
-    img_url: ''
-  }
+    name: 'Микрофон Handel K11',
+    price: 6800,
+    description: 'тип: конденсаторный конструкция: петличный (клипса) назначение: универсальный тип подключения: беспроводной радиосистема: Нет разъемы: Lightning цвет: черный',
+    url: 'https://kaspi.kz/shop/p/handel-k11-106313925/?c=750000000#!/item',
+    img_url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h18/h1e/62189525499934/handel-k11-2-sht-106313925-1.jpg'
+  },
+  {
+    id: 4,
+    name: 'Утюг Vitek VT-8304 черный',
+    price: 14990,
+    description: 'мощность: 2400 Вт резервуар для воды: 350 мл автоматическое отключение: Да защита от накипи: Да противокапельная система: Да цвет: черный',
+    url: 'https://kaspi.kz/shop/p/vitek-vt-8304-chernyi-3800548/?c=750000000#!/item',
+    img_url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h7a/h70/31593847128094/vitek-vt-8304-cernyj-sinij-3800548-1-Container.jpg'
+  },
+  {
+    id: 5,
+    name: 'Утюг Vitek VT-8304 черный',
+    price: 480550,
+    description: 'мощность: 2400 Вт резервуар для воды: 350 мл автоматическое отключение: Да защита от накипи: Да противокапельная система: Да цвет: черный',
+    url: 'https://kaspi.kz/shop/p/vitek-vt-8304-chernyi-3800548/?c=750000000#!/item',
+    img_url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h7a/h70/31593847128094/vitek-vt-8304-cernyj-sinij-3800548-1-Container.jpg'
+  },
+  {
+    id: 6,
+    name: 'Утюг Vitek VT-8304 черный',
+    price: 14990,
+    description: 'мощность: 2400 Вт резервуар для воды: 350 мл автоматическое отключение: Да защита от накипи: Да противокапельная система: Да цвет: черный',
+    url: 'https://kaspi.kz/shop/p/vitek-vt-8304-chernyi-3800548/?c=750000000#!/item',
+    img_url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h7a/h70/31593847128094/vitek-vt-8304-cernyj-sinij-3800548-1-Container.jpg'
+  },
+  {
+    id: 7,
+    name: 'Утюг Vitek VT-8304 черный',
+    price: 14990,
+    description: 'мощность: 2400 Вт резервуар для воды: 350 мл автоматическое отключение: Да защита от накипи: Да противокапельная система: Да цвет: черный',
+    url: 'https://kaspi.kz/shop/p/vitek-vt-8304-chernyi-3800548/?c=750000000#!/item',
+    img_url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h7a/h70/31593847128094/vitek-vt-8304-cernyj-sinij-3800548-1-Container.jpg'
+  },
+  {
+    id: 8,
+    name: 'Утюг Vitek VT-8304 черный',
+    price: 14990,
+    description: 'мощность: 2400 Вт резервуар для воды: 350 мл автоматическое отключение: Да защита от накипи: Да противокапельная система: Да цвет: черный',
+    url: 'https://kaspi.kz/shop/p/vitek-vt-8304-chernyi-3800548/?c=750000000#!/item',
+    img_url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h7a/h70/31593847128094/vitek-vt-8304-cernyj-sinij-3800548-1-Container.jpg'
+  },
+  {
+    id: 9,
+    name: 'Утюг Vitek VT-8304 черный',
+    price: 14990,
+    description: 'мощность: 2400 Вт резервуар для воды: 350 мл автоматическое отключение: Да защита от накипи: Да противокапельная система: Да цвет: черный',
+    url: 'https://kaspi.kz/shop/p/vitek-vt-8304-chernyi-3800548/?c=750000000#!/item',
+    img_url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h7a/h70/31593847128094/vitek-vt-8304-cernyj-sinij-3800548-1-Container.jpg'
+  },
+  {
+    id: 10,
+    name: 'Утюг Vitek VT-8304 черный',
+    price: 14990,
+    description: 'мощность: 2400 Вт резервуар для воды: 350 мл автоматическое отключение: Да защита от накипи: Да противокапельная система: Да цвет: черный',
+    url: 'https://kaspi.kz/shop/p/vitek-vt-8304-chernyi-3800548/?c=750000000#!/item',
+    img_url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h7a/h70/31593847128094/vitek-vt-8304-cernyj-sinij-3800548-1-Container.jpg'
+  },
 ];
 
 
